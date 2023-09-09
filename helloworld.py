ffd27fa0d2f17a913bc75dab05169cb988821fe0
JohnLYYN123 <96747053+JohnLYYN123@users.noreply.github.com>
2023-09-09 07:00:30 -0400

    commit 4

--- helloworld.py
+++ helloworld.py
@@ -4,5 +4,9 @@ print("Hello World, I am Yihang, and I have been at U of T for four years")
 print("my favorite food is ice cream")
 print("my favorite computer language is Python")
 
+<<<<<<< HEAD
 # comment about commit 2
 
+=======
+print("this is commit 4")
+>>>>>>> e5a4a6b (commit 4)
